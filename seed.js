cec40a71630fef302ba0112c64167374caa2eefb
DHoper <gxgemini777@gmail.com>
2023-07-07 15:08:25 +0800

    revise pre7day 抓取方式

--- seed.js
+++ seed.js
@@ -23,29 +23,49 @@ function getRandomInt(min, max) {
 
 async function sowing() {
   await StoredData.deleteMany({})
-  .then(() => console.log("成功刪除文檔!"))
-  .catch((err) => console.log("文檔刪除失敗!", err));
+    .then(() => console.log("成功刪除文檔!"))
+    .catch((err) => console.log("文檔刪除失敗!", err));
 
-for (let i = 1; i < 8; i++) {
-  let day = `2023-07-0${i}`;
-  let htvalue = [];
+  for (let i = 1; i < 31; i++) {
+    let day = `2023-06-${i.toString().padStart(2, "0")}`;
+    let htvalue = [];
 
-  for (let j = 0; j < 24; j++) {
-    htvalue.push({
-      temp: getRandomInt(23, 35),
-      humid: getRandomInt(40, 60),
-      hour: j,
-    });
-    console.log(i, j);
+    for (let j = 0; j < 24; j++) {
+      htvalue.push({
+        temp: getRandomInt(23, 35),
+        humid: getRandomInt(40, 60),
+        hour: j,
+      });
+      console.log(i, j);
+    }
+
+    await StoredData.create({
+      date: day,
+      HTValue: htvalue,
+    })
+      .then(() => console.log("種子植入成功!", i))
+      .catch((err) => console.log(err));
   }
 
-  await StoredData.create({
-    date: day,
-    HTValue: htvalue,
-  })
-    .then(() => console.log("種子植入成功!", i))
-    .catch((err) => console.log(err));
-}
+  for (let i = 1; i < 8; i++) {
+    let day = `2023-07-${i.toString().padStart(2, "0")}`;
+    let htvalue = [];
 
+    for (let j = 0; j < 24; j++) {
+      htvalue.push({
+        temp: getRandomInt(23, 35),
+        humid: getRandomInt(40, 60),
+        hour: j,
+      });
+      console.log(i, j);
+    }
+
+    await StoredData.create({
+      date: day,
+      HTValue: htvalue,
+    })
+      .then(() => console.log("種子植入成功!", i))
+      .catch((err) => console.log(err));
+  }
 }
 sowing();
